b3fb98c05bd821201ca0ad1349e7981a73e113c2
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-10-12 05:26:50 +0530

    worked more on error handling

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,33 @@
 const { restart } = require('nodemon');
+const errorres = require('../utils/errorres');
 
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack);
+  console.log(err);
+
+  let error = { ...err };
+  error.message = err.message;
+
+  // Bad object id in mongodb
+  if (err.name === 'CastError') {
+    const message = `Resorce not found with id of ${err.value}`;
+    error = new errorres(message, 404);
+  }
+
+  // If duplicate key
+  if (err.name === 'MongoError' && err.code === 11000) {
+    const message = `Duplicate key value found`;
+    error = new errorres(message, 400);
+  }
+
+  // ValidationError
+  if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map(val => val.message);
+      error = new errorres(message, 400);   
+  }
 
   res
-    .status(err.statusCode || 500)
-    .json({ message: err.message || `Server error` });
+    .status(error.statusCode || 500)
+    .json({ message: error.message || `Server error` });
 };
 
 module.exports = errorHandler;
